02abf989613346b69f920625ed50bb1fadf08bfd
yusungill <chinggin@navcer.com>
2020-06-28 21:00:08 +0900

    jQuery로 css변경하기

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,14 @@
 
 $(function(){
 
-    $('h1').css( {'color' : 'red'});
+
+    $('h1').css({
+       color : 'red',
+       textDecoration : 'underline',
+       borderBottom:'5px solid red',
+       transform :'rotate(45deg)',
+       opacity : 0.5
+    })
+
+
 });
