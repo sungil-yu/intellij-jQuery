c46ae0342da61094fee10ac4b9b22c2833f1734a
yusungill <chinggin@navcer.com>
2020-06-30 06:09:40 +0900

    jQuery animate(2)

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -13,8 +13,14 @@ $(function(){
    $(function(){
     $('#typo .inner').click(function(){
         //선택자.animate({속성:값, 속성:값}, 시간, 이징, 다른할일)
+        //제이쿼리 이징 , linear, swing
+        //function(){실제 할일}
+        $(this).animate({opacity:0,fontSize:'0px'},1500 ,'swing',
+        function(){
 
-        $(this).animate({opacity:0,fontSize:'0px'},1500);
+           $(this).animate({opacity:1,fontSize : '110px'}, 500);
+
+        });
 
     });
 
